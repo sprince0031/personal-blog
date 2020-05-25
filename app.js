db5f0e14b5cd7e7434a58df711d185b15c7f91c0
sprince0031 <siddharthprince31@gmail.com>
2020-05-25 21:28:29 +0530

    Added theme toggling functionality

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const posts = [];
+let darkTheme = false;
 // let postId = 0;
 
 const app = express();
@@ -19,20 +20,24 @@ app.use(express.static("public"));
 
 app.get('/', (req, res) => {
 
-  res.render('home', {homeStartingContent: homeStartingContent, posts: posts});
+  res.render('home', {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+    darkTheme: darkTheme
+  });
 
 })
 
 app.get('/about', (req, res) => {
-  res.render('about', {aboutContent: aboutContent});
+  res.render('about', {aboutContent: aboutContent, darkTheme: darkTheme});
 })
 
 app.get('/contact', (req, res) => {
-  res.render('contact', {contactContent: contactContent});
+  res.render('contact', {contactContent: contactContent, darkTheme: darkTheme});
 })
 
 app.get('/compose', (req, res) => {
-  res.render('compose');
+  res.render('compose', {darkTheme: darkTheme});
 })
 
 app.post('/compose', (req, res) => {
@@ -49,18 +54,43 @@ app.post('/compose', (req, res) => {
 })
 
 app.get('/posts/:postName', (req, res) => {
-  const objToPass = posts.find(post => {
+  let objToPass = posts.find(post => {
     return post.title === req.params.postName;
   });
+  
   if (objToPass) {
+    objToPass.darkTheme = darkTheme;
     res.render('post', objToPass);
   } else {
-    res.render('404');
+    res.render('404', {darkTheme: darkTheme});
   }
   
 })
 
+app.post('/themetoggle', (req, res) => {
+  darkTheme = req.body.darkTheme;
+  
+  res.send(darkTheme);
+})
 
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', {darkTheme: darkTheme});
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: '404, Not found!' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('404, Not found!');
+});
 
 
 app.listen(process.env.PORT || 3000, function() {
