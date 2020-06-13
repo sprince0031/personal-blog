7f020336cb1928544e67ffa1dc51acd12a0d2415
sprince0031 <siddharthprince31@gmail.com>
2020-06-14 00:33:25 +0530

    reworked theme switching to use cookies so that settings will apply locally on client side and also tweaked design a little.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const posts = [];
-let darkTheme = false;
 // let postId = 0;
 
 const app = express();
@@ -23,21 +22,20 @@ app.get('/', (req, res) => {
   res.render('home', {
     homeStartingContent: homeStartingContent,
     posts: posts,
-    darkTheme: darkTheme
   });
 
 })
 
 app.get('/about', (req, res) => {
-  res.render('about', {aboutContent: aboutContent, darkTheme: darkTheme});
+  res.render('about', {aboutContent: aboutContent});
 })
 
 app.get('/contact', (req, res) => {
-  res.render('contact', {contactContent: contactContent, darkTheme: darkTheme});
+  res.render('contact', {contactContent: contactContent});
 })
 
 app.get('/compose', (req, res) => {
-  res.render('compose', {darkTheme: darkTheme});
+  res.render('compose');
 })
 
 app.post('/compose', (req, res) => {
@@ -59,18 +57,11 @@ app.get('/posts/:postName', (req, res) => {
   });
   
   if (objToPass) {
-    objToPass.darkTheme = darkTheme;
     res.render('post', objToPass);
   } else {
-    res.render('404', {darkTheme: darkTheme});
+    res.render('404');
   }
   
-})
-
-app.post('/themetoggle', (req, res) => {
-  darkTheme = req.body.darkTheme;
-  
-  res.send(darkTheme);
 })
 
 app.use(function(req, res, next){
@@ -78,7 +69,7 @@ app.use(function(req, res, next){
 
   // respond with html page
   if (req.accepts('html')) {
-    res.render('404', {darkTheme: darkTheme});
+    res.render('404');
     return;
   }
 
